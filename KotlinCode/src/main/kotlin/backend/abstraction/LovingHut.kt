6a06d776b237c77aa8e40006a86c75f9adf7fffc
Aymen K <aymen@Aymens-MacBook-Air.local>
2023-08-20 01:08:30 -0700

    release-1.0 added a Rstaurant class

---
+++ KotlinCode/src/main/kotlin/backend/abstraction/LovingHut.kt
@@ -0,0 +1,18 @@
+package backend.abstraction
+
+class LovingHut: Restaurant(4.3,"Vegan") {
+    fun greeting(){
+        println("Welcome to Loving Hut")
+    }
+    override fun parking(){
+        println("Far drive: ~1.5 hours")
+    }
+
+    override fun favoriteMenuItems() {
+        val foods = listOf("Potstickers",
+            "Basil Fried Rice",
+            "Lucky Curry",
+            "General Tso")
+        println(foods)
+    }
+}
